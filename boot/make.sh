857267d79ac37bce126ef3ce672a5d2192b6c5f6
Froser <hellofroser@hotmail.com>
2020-06-11 08:54:52 +0800

    初步实现寻找loader.bin的逻辑。

--- boot/make.sh
+++ boot/make.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+mkdir -p ../bin
+
 echo "Building boot.img..."
-dd if=/dev/zero of=boot.img bs=512 count=2880
+dd if=/dev/zero of=../bin/boot.img bs=512 count=2880
 
 echo "Compiling floppy_boot.S..."
-nasm floppy_boot.S -o boot.bin
+nasm floppy_boot.S -o ../bin/boot.bin
 
 echo "Writing to boot.img..."
-dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
+dd if=../bin/boot.bin of=../bin/boot.img bs=512 count=1 conv=notrunc
 
-echo "Cleaning..."
-rm boot.bin
+echo "Compiling loader.S..."
+nasm loader.S -o ../bin/loader.bin
+
+echo "Mounting loader.bin..."
+mount ../bin/boot.img /media/ -t vfat -o -loop
+cp ../bin/loader.bin /media/
+sync
+umount /media/
 
 echo "Done."
